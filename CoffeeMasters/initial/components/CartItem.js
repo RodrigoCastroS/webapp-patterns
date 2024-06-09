a53c750c0391a7597f4bfaa98c1ace647aae1e2b
Rodrigo C <rodrigocsastre@gmail.com>
2024-06-09 13:54:17 +0200

    feat: create an interpolation function that can be used as a Singleton or a Factory pattern

--- CoffeeMasters/initial/components/CartItem.js
+++ CoffeeMasters/initial/components/CartItem.js
@@ -9,14 +9,26 @@ export default class CartItem extends HTMLElement {
         const item = JSON.parse(this.dataset.item);
         this.innerHTML = ""; // Clear the element
 
+    
+        // This fn can be extracted as helper so it can be used anywhere. It's a bit hacky,
+        // but the purpose is to have easier dynamic data.
+
+        function interpolate(str, params){
+            const names = Object.keys(params); // qty, name, price
+            const values = Object.values(params); // 1, 'hot coffee', '2,99'
+            const body = `return \`${str}\`;`;
+            return new Function(...names, `return \`${str}\`;`)(...values); // This fn is autoInvoqued when calling interpolated.
+        }
+
+
         const template = document.getElementById("cart-item-template");
-        const content = template.content.cloneNode(true);
+        this.innerHTML = interpolate(template.innerHTML, {
+            qty: `${item.quantity}x`, 
+            price: `$${item.product.price.toFixed(2)}`,
+            name: item.product.name
+        });
 
-        this.appendChild(content);    
-
-        this.querySelector(".qty").textContent = `${item.quantity}x`;
-        this.querySelector(".name").textContent = item.product.name;
-        this.querySelector(".price").textContent = `$${item.product.price.toFixed(2)}`;
+    
         this.querySelector("a.delete-button").addEventListener("click", event => {
             removeFromCart(item.product.id);
         })
